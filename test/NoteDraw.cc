aae5bd73289e952e2248667e6385ad9738b70046
skjsjhb <113125446+skjsjhb@users.noreply.github.com>
2023-04-15 13:01:13 +0800

    test(*): wrap all test code
    - Display all notes to show.
    - Update slot texture.
    - Increase hashi split part size.

--- test/NoteDraw.cc
+++ test/NoteDraw.cc
@@ -12,20 +12,12 @@ int main()
     // Create a note. Verified in NotePos.cc
     Tapu t;
     Shizuku k;
-    Slot s;
     Puresu r;
     Hoshi h;
     Hashi a;
+    Slot s;
 
     bool running = true;
-    r.isFake = false;
-    r.isVisible = true;
-    r.targetSlot = &s;
-    r.autoControl = true;
-    r.keyCode = 87;
-    r.basePosition[0] = r.basePosition[1] = r.basePosition[2] = 0;
-    r.hitTime = 4;
-    r.absLength = 2;
 
     t.hitTime = 2;
     t.isFake = false;
@@ -34,28 +26,35 @@ int main()
     t.autoControl = true;
     t.keyCode = 87; // Key 'W'
 
-    a.hitTime = 2;
-    a.isFake = false;
-    a.isVisible = true;
-    a.targetSlot = &s;
-    a.autoControl = true;
-    a.keyCode = 87;
-    a.basePosition[0] = a.basePosition[1] = a.basePosition[2] = 0;
-    a.absLength = a.length = 4;
+    k.hitTime = 4;
+    k.isFake = false;
+    k.isVisible = true;
+    k.targetSlot = &s;
+    k.autoControl = true;
+    k.keyCode = 87;
 
-    h.hitTime = 2;
+    r.isFake = false;
+    r.isVisible = true;
+    r.targetSlot = &s;
+    r.autoControl = true;
+    r.keyCode = 87;
+    r.hitTime = 6;
+    r.absLength = 2;
+
+    h.hitTime = 10;
     h.isFake = false;
     h.isVisible = true;
     h.targetSlot = &s;
     h.autoControl = true;
     h.keyCode = 87;
 
-    k.hitTime = 3;
-    k.isFake = false;
-    k.isVisible = true;
-    k.targetSlot = &s;
-    k.autoControl = true;
-    k.keyCode = 87;
+    a.hitTime = 12;
+    a.isFake = false;
+    a.isVisible = true;
+    a.targetSlot = &s;
+    a.autoControl = true;
+    a.keyCode = 87;
+    a.absLength = 2;
 
     s.center[0] = s.center[1] = 0;
     s.center[2] = 0;
@@ -64,6 +63,7 @@ int main()
     s.up[1] = 1;
     s.up[0] = s.up[2] = 0;
     s.isVisible = true;
+
     DrawContext ctx;
     vec3 camPos = {0, -6, 9}, camDir = {0, 0.8, -1}, camUp = {0, 1, 0.8};
     ctx.cam.setState(camPos, camDir, camUp, glm_rad(60), 1920.0 / 1080);
@@ -91,44 +91,56 @@ int main()
 
     vtGraphicsInit();
     inputs.setupInputListeners();
-    unsigned int i = 0;
     double time = 0;
     while (running)
     {
         inputs.pollInputEvents();
         time = vtGetTime();
-        a.basePosition[2] = 8 - vtGetTime();
-        if (time > 8)
+        if (time > 16)
         {
-            //  break;
+            break;
+        }
+        if (time < 2)
+        {
+            s.center[0] = -4;
+        }
+        else if (time > 2 && time < 4)
+        {
+            s.center[0] = -2;
+        }
+        else if (time > 4 && time < 8)
+        {
+            s.center[0] = 0;
+        }
+        else if (time > 8 && time < 10)
+        {
+            s.center[0] = 2;
+        }
+        else if (time > 10 && time < 14)
+        {
+            s.center[0] = 4;
         }
         ctx.polygons.clear();
 
-        // p.tick(time);
-
-        // h.basePosition[1] = 0;
-        // h.basePosition[0] = 0;
-        // h.basePosition[2] = 2;
         glm_vec3_normalize(s.up);
         s.tick(time);
         t.performJudge(time, inputs, sm);
         t.tick(time);
-        // k.performJudge(time, inputs, sm);
-        // k.tick(time);
-        // r.performJudge(time, inputs, sm);
-        // r.tick(time);
-        // h.performJudge(time, inputs, sm);
-        // h.tick(time);
-        // a.performJudge(time, inputs, sm);
-        // a.tick(time);
+        k.performJudge(time, inputs, sm);
+        k.tick(time);
+        r.performJudge(time, inputs, sm);
+        r.tick(time);
+        h.performJudge(time, inputs, sm);
+        h.tick(time);
+        a.performJudge(time, inputs, sm);
+        a.tick(time);
 
-        // p.draw(ctx);
         s.draw(ctx);
-        //      r.draw(ctx);
-        // a.draw(ctx);
+        r.draw(ctx);
+        a.draw(ctx);
         t.draw(ctx);
-        //  k.draw(ctx);
-        // h.draw(ctx);
+        k.draw(ctx);
+        h.draw(ctx);
         vtDraw(ctx);
         vtWindowLoop();
     }
